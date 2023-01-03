6796358ea2b3f1f105820ab27e327d1177dfe107
Beto Rod <beto.rod@gatech.edu>
2023-01-03 23:20:34 +0000

    Added counter for tests

--- Test/TestFramework.cpp
+++ Test/TestFramework.cpp
@@ -20,6 +20,7 @@ typedef std::map<int, callback_t> callback_map_t;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	int iTestTotal = 0, iTestOK = 0, iTestNotOK = 0;
 	callback_map_t callbackMap;  // Declare the dictionary
 	callbackMap[0] = &Tests::test001;
 	callbackMap[1] = &Tests::test002;
@@ -35,10 +36,18 @@ int _tmain(int argc, _TCHAR* argv[])
 	for (callback_map_t::iterator it = callbackMap.begin(); it != callbackMap.end(); ++it) {
 		std::cout << "Running test: " << it->first + 1 << std::endl;
 		if(it->second())
+		{
 			std::cout << "Test OK" << std::endl;
+			iTestOK++;
+		}
 		else
+		{
 			std::cout << "Test Fail" << std::endl;
+			iTestNotOK++;
+		}
+		iTestTotal++;
 	}
+    std::cout << "Total Tests: " << iTestTotal << " OK = " << iTestOK << " NOT OK = " << iTestNotOK << std::endl;
     std::cout << "Press any key to continue... ";
 	getchar();
 	return 0;
